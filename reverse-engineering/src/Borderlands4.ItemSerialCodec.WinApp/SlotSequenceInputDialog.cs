8d286f03e7d2c466f718cde1e92fda228936e343
zjfeiye <zjfeiye@live.com>
2025-10-24 11:05:20 +0800

    修正一个发布问题

--- reverse-engineering/src/Borderlands4.ItemSerialCodec.WinApp/SlotSequenceInputDialog.cs
+++ reverse-engineering/src/Borderlands4.ItemSerialCodec.WinApp/SlotSequenceInputDialog.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.ComponentModel;
 
 namespace Borderlands4.ItemSerialCodec.WinApp
 {
     public partial class SlotSequenceInputDialog : Form
     {
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public int SequenceNumber { get; set; }
 
         public SlotSequenceInputDialog()
